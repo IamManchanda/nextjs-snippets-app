01770fa0fa3b1aa1e9b0e25db2b7660a726837c1
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-11-06 16:13:26 +0530

    More config changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-import '../styles/app.css';
+import "../styles/app.scss";
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <div className="bg-red-600 w-full p-10 min-h-screen">
-            <div className="max-w-2xl mx-auto">
-                <Component {...pageProps} />
-            </div>
-        </div>
-    );
+  return (
+    <div className="bg-red-600 w-full p-10 min-h-screen">
+      <div className="max-w-2xl mx-auto">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
 export default MyApp;
