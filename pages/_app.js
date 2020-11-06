9f6c6b351479f8e3c57affd0870ed090ac085c5f
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-11-06 16:49:41 +0530

    Update Styling

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "../styles/app.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="bg-red-600 w-full p-10 min-h-screen">
+    <div className="app-background w-full p-10 min-h-screen">
       <div className="max-w-2xl mx-auto">
         <Component {...pageProps} />
       </div>
