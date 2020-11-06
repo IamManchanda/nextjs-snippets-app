cfd08abb7b913c7299486d0e0772d5c08773ba4f
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-11-06 16:52:05 +0530

    Code formatting

--- pages/api/snippets.js
+++ pages/api/snippets.js
@@ -1,13 +1,13 @@
-import { getSnippets } from '../../utils/Fauna';
+import { getSnippets } from "../../utils/Fauna";
 export default async function handler(req, res) {
-    if (req.method !== 'GET') {
-        return res.status(405);
-    }
+  if (req.method !== "GET") {
+    return res.status(405);
+  }
 
-    try {
-        //TODO: get snippets
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ msg: 'Something went wrong.' });
-    }
+  try {
+    //TODO: get snippets
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "Something went wrong." });
+  }
 }
